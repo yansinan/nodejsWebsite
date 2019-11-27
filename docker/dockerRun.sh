2de16f87ad6c45e09b341165bb13a5b4cf2b2264
Dr <yansinan@163.com>
2019-11-27 20:36:53 +0800

    dockerRun

--- docker/dockerRun.sh
+++ docker/dockerRun.sh
@@ -4,7 +4,15 @@ sudo docker run -it  -p 8084:8080 \
     node/dora:base
 
 
-sudo docker run -it  -p 8084:8080 \
---name doraAlpine --network=nginxNet  --restart=always \
+sudo docker run -it  -p 8084:8080 -p 9229:9229\
+--name doraAlpine --network=nginxNet \
+--rm \
+--restart=always \
+-v /home/dr/workspace/nodejsWebsite:/home/node \
+node/dora:alpine
+
+sudo docker run -it  -p 8084:8080 -p 9229:9229 \
+--name=dora --network=nginxNet \
+--rm \
 -v /home/dr/workspace/nodejsWebsite:/home/node \
 node/dora:alpine
