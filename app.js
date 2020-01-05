f474f8f52a3b60d58eb50f55c850c1f75cf4bf00
Dr <yansinan@163.com>
2020-01-05 15:33:31 +0800

    忽略列表，系统备注

--- app.js
+++ app.js
@@ -20,7 +20,19 @@ class AppBootHook {
     }
 
     async didLoad() {
+        // const directory = path.join(this.app.config.baseDir, 'app/model');
+        // this.app.loader.loadToContext(directory, 'model');
+        
+        // 获取所有的 loadUnit
+        // const servicePaths = this.app.loader.getLoadUnits().map(unit => path.join(unit.path, 'app/model'));
 
+        // this.app.loader.loadToContext(servicePaths, 'model', {
+        //     // service 需要继承 app.Service，所以要拿到 app 参数
+        //     // 设置 call 在加载时会调用函数返回 UserService
+        //     call: true,
+        //     // 将文件加载到 app.model
+        //     fieldClass: 'model',
+        // });
     }
 
     async willReady() {
