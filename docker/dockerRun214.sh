52b3241e9ca03cb3ff3e1b292a7013ff84caf931
Dr <yansinan@163.com>
2020-01-05 15:30:35 +0800

    docker更新

--- docker/dockerRun214.sh
+++ docker/dockerRun214.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-sudo docker run -it  -p 8084:8080 -p 9229:9229 \
+sudo docker run -it  -p 8084:8080 -p 8900:8900 -p 8899:8899 -p 8888:8888 -p 9229:9229 -p 9999:9999 -p 5800:5800 -p 9230:9230 \
 --name=dora --network=nginxNet \
---rm \
 -v /home/dr/workspace/dora214:/home/node \
 node/dora:214 \
 /bin/bash
+
+# sudo docker run -it  -p 8084:8080 -p 9229:9229 \
+# --name=dora --network=host \
+# -v /home/dr/workspace/dora214:/home/node \
+# node/dora:214 \
+# /bin/bash
