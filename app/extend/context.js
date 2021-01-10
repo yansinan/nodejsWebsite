b09f568361535d088b0f6df04ef38dee19f63cf9
Dr <yansinan@163.com>
2021-01-09 20:20:17 -0800

    tooltip版人物名

--- app/extend/context.js
+++ app/extend/context.js
@@ -292,7 +292,9 @@ module.exports = {
         pageData.site = await this.getSiteInfo();
         pageData.staticRootPath = this.app.config.static.prefix;
         pageData.staticThemePath = this.app.config.static.prefix + '/themes/' + defaultTemp.alias;
-
+        // 人物（艺术家）列表
+        pageData.listAvatars = (await ctx.helper.reqJsonData('artist/getList', {filesType:"navAvatar"})).docs;
+        
         // 针对分类页和内容详情页动态添加meta
         // let defaultTempItems = defaultTemp.items;
         if (!_.isEmpty(pageData.site)) {
