7520100d416e97474ff0e320139186ba559270e8
Dr <yansinan@163.com>
2021-01-31 09:07:31 -0800

    Video.model初始定义

---
+++ app/model/Video.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Dr on 2020/12/19.
+ * 演出信息
+ */
+
+module.exports =app=>{
+    {
+        const mongoose = app.mongoose
+        var Schema = mongoose.Schema;
+        var moment = require('moment')
+
+        // 继承自Doc        
+        let Doc=app.model.Doc || require("./Doc")(app) || INIT_DOC(app);//
+        
+        let optionsSchema={
+            toJSON:{
+                getters: true,
+                virtuals: true
+            },
+            toObject:{
+                getters: true,
+                virtuals: true
+            },
+        }
+        var schema = new Schema({
+
+        });        
+
+        // url地址
+        schema.virtual('url').get(function () {
+            return `/video/${this._id}.html`;
+        });
+        // 演出标题
+        schema.virtual('title').get(function () {
+            return this.name;
+        }).set(function(v){
+            this.name=v;
+        });
+        // 演出短标题
+        schema.virtual('sTitle').get(function () {
+            return this.alias;
+        }).set(function(v){
+            this.alias=v;
+        });
+        schema.virtual('listArtists').get(function () {
+            return this.listRefs;
+        });
+
+        let model=app.model.Video || Doc.discriminator("Video", schema);
+    
+        return model
+    }
+
+}
