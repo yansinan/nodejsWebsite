efdb8e7d288ad828f4d3d7d9b158249e08d92d70
Dr <yansinan@163.com>
2021-02-04 06:11:49 -0800

    imgFit前端处理图片大小

--- backstage/artist/src/views/common/ListVideos.vue
+++ backstage/artist/src/views/common/ListVideos.vue
@@ -27,7 +27,7 @@
                   <span slot="prepend" v-else style="text-align:center;font-size: 18px;" ><i class="el-icon-link"/>视频 {{index+1}}</span>
                   <!-- <el-button slot="append" icon="el-icon-delete" @click.prevent="removeDomain(domain)"/> -->
                 </el-input>
-              <el-image :span="24" :src="domain.urlImg" :fit="contain"/>
+              <img width="100%" :src="domain.urlImg" fit="contain" @load="eImgLoaded" crossOrigin="Anonymous"/>
               <div class="titleVideo" style="">
                 <span v-if="domain.name">{{domain.name}}</span>
                 <el-button type="danger" plain slot="append" icon="el-icon-delete" @click.prevent="removeDomain(domain)"/>
@@ -85,6 +85,7 @@
 </style>
 <script>
   import { updateOne } from "@root/publicMethods/apiGeneral";
+  import { imgFit } from "@root/publicMethods/imgFit";
   const objURLDefault={
     name:"",
     type:"link:music.163.com",
@@ -131,58 +132,58 @@
     },
     methods: {
       // 手动同步网易云的乐队MV
-        eBnSyncNCM(e){
-          let that=this;
-          fetch("/api/artist/fetchNCMMV?id="+this.dialogState.formData._id)
-          .then((data)=>data.text())
-          .then((data)=> {
-              // 在这个then里面我们能拿到最终的数据
-              let res=JSON.parse(data);
-              if(res.status==200 && res.data.length>0){
-               
-                //去重 & 合并
-                // let list=res.data.concat(that.listObjURL);
-                // list=[...new Set(list)];
-                // that.listObjURL.splice(0,that.listObjURL.length,...list);
-                let msg="";
-                let listNew=res.data.filter(vNew=>{
-                  let isOld= (that.listObjURL.find(vOld=>(vNew.idURL==vOld.idURL)));
-                  if(isOld)msg+=vNew.name+","
-                  return !isOld;
-                })
-                msg="剔除重复"+listNew.length+"条V,新增:"+msg;
-                that.listObjURL.unshift(...listNew);
-                // if(listNew.length<res.data.length)that.$message({
-                //   message: msg,
-                //   type: "success"
-                // });
-                that.$message({
-                  message: "找到网易"+res.data.length+"条。"+msg,
-                  type: "success"
-                });
-                // 去空数据
-                // that.listObjURL=that.listObjURL.filter(v=>(v));
-                // 触发事件;
-                if(that.listObjURL)that.$emit('list-changed',that.listObjURL);
+      eBnSyncNCM(e){
+        let that=this;
+        fetch("/api/artist/fetchNCMMV?id="+this.dialogState.formData._id)
+        .then((data)=>data.text())
+        .then((data)=> {
+            // 在这个then里面我们能拿到最终的数据
+            let res=JSON.parse(data);
+            if(res.status==200 && res.data.length>0){
+              
+              //去重 & 合并
+              // let list=res.data.concat(that.listObjURL);
+              // list=[...new Set(list)];
+              // that.listObjURL.splice(0,that.listObjURL.length,...list);
+              let msg="";
+              let listNew=res.data.filter(vNew=>{
+                let isOld= (that.listObjURL.find(vOld=>(vNew.idURL==vOld.idURL)));
+                if(isOld)msg+=vNew.name+","
+                return !isOld;
+              })
+              msg="剔除重复"+listNew.length+"条V,新增:"+msg;
+              that.listObjURL.unshift(...listNew);
+              // if(listNew.length<res.data.length)that.$message({
+              //   message: msg,
+              //   type: "success"
+              // });
+              that.$message({
+                message: "找到网易"+res.data.length+"条。"+msg,
+                type: "success"
+              });
+              // 去空数据
+              // that.listObjURL=that.listObjURL.filter(v=>(v));
+              // 触发事件;
+              if(that.listObjURL)that.$emit('list-changed',that.listObjURL);
 
-              }else{
-                that.$message.error(
-                  that.$t("validate.inputCorrect", { label: "网易云音乐获取MV" })
-                );
-              }
-          }).catch(e=>{
-            debugger
-            that.$message.error(
-              that.$t("validate.inputCorrect", { label: "网易云音乐获取MV" })
-            );
-            console.error(e)
-          })
+            }else{
+              that.$message.error(
+                that.$t("validate.inputCorrect", { label: "网易云音乐获取MV" })
+              );
+            }
+        }).catch(e=>{
+          debugger
+          that.$message.error(
+            that.$t("validate.inputCorrect", { label: "网易云音乐获取MV" })
+          );
+          console.error(e)
+        })
 
-        },
-        // 弹窗打开时
-        eDialogOpen(e){
-   
-        },
+      },
+      // 弹窗打开时
+      eDialogOpen(e){
+  
+      },
       // 手动更新到服务器
       submitUpload(){
         let that=this;
@@ -308,6 +309,22 @@
           }
         });
       },
+      // 图片读取完成，开始裁切、缩小
+      eImgLoaded(e){
+        let targetWidth=1920;
+        let targetHeight=1080;
+
+        let img=e.target;
+        let typeSrc=img.src.substring(0,7);
+        let time=new Date();
+        // if(typeSrc.indexOf("data:")==-1 && typeSrc.indexOf("blob:")==-1)
+        if(img.naturalWidth!=targetWidth || img.naturalHeight!=targetHeight)
+        imgFit(img,targetWidth,targetHeight).then(resSrc=>{
+          img.src= resSrc;
+          console.log("图片裁切完成",(new Date()-time),typeSrc)
+        });//网易云MV图尺寸628,353
+      },
+
     }
   }
 </script>
---
+++ backstage/publicMethods/imgFit.js
@@ -0,0 +1,30 @@
+
+
+      
+
+module.exports = {
+    imgFit(img,width,height,fit="cover"){
+        return new Promise((resolve,reject)=>{
+          let resSrc=""
+
+          let canvas = document.createElement('canvas');
+          var ctx = canvas.getContext('2d');
+          canvas.width = width;
+          canvas.height = height;
+          // get the scale
+          var scale = 1;
+          if(fit=="cover")scale=Math.max(canvas.width / img.width, canvas.height / img.height);
+          if(fit=="fill")scale=Math.min(canvas.width / img.width, canvas.height / img.height);
+          // get the top left position of the image
+          var x = (canvas.width / 2) - (img.width / 2) * scale;
+          var y = (canvas.height / 2) - (img.height / 2) * scale;
+          ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+          // 输出0.95的是最接近原图的压缩比，//98 到 99 尤其是 99 到 100，增长突然变陡，比原图大小翻了将近 3 倍
+          // resSrc = canvas.toDataURL('image/jpeg', 0.95);
+          canvas.toBlob(blob=>{
+              resSrc=URL.createObjectURL(blob);
+              resolve(resSrc);
+          });          
+        })
+    }
+}
