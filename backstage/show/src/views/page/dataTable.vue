c5382c117ce1fa7f1b5502d1b78db00494adb5ce
Dr <yansinan@163.com>
2021-04-08 01:03:08 -0700

    演出活动模块升级：TODO：后台界面优化

--- backstage/show/src/views/page/dataTable.vue
+++ backstage/show/src/views/page/dataTable.vue
@@ -6,62 +6,60 @@
       ref="multipleTable"
       :data="dataList"
       tooltip-effect="dark"
-      style="width: 100%"
+      style="width: 100%"      
+      class="tableBox"
       @selection-change="handleContentSelectionChange"
     >
       <el-table-column type="selection" width="30"></el-table-column>
       <el-table-column prop="isTop" :label="$t('contents.rec')" width="30" show-overflow-tooltip>
         <template slot-scope="scope">
-          <svg-icon
-            :style="yellow"
-            v-show="scope.row.isTop === 1"
-            @click="topContent(scope.$index, dataList)"
-            icon-class="icon_star_fill"
-          />
-          <svg-icon
-            :style="gray"
-            v-show="scope.row.isTop != 1"
-            @click="topContent(scope.$index, dataList)"
-            icon-class="icon_star"
-          />
+          <svg-icon :style="yellow" v-show="scope.row.isTop === 1" @click="topContent(scope.$index, dataList)" icon-class="icon_star_fill" />
+          <svg-icon :style="gray" v-show="scope.row.isTop != 1" @click="topContent(scope.$index, dataList)" icon-class="icon_star" />
         </template>
       </el-table-column>
-      <el-table-column
-        prop="roofPlacement"
-        :label="$t('contents.roofPlacement')"
-        width="30"
-        show-overflow-tooltip
-      >
+      <el-table-column prop="roofPlacement" :label="$t('contents.roofPlacement')" width="30" show-overflow-tooltip>
         <template slot-scope="scope">
-          <svg-icon
-            :style="green"
-            v-show="scope.row.isTop === 1 && scope.row.roofPlacement == 1"
-            @click="roofContent(scope.$index, dataList)"
-            icon-class="icon_ping"
-          />
-          <svg-icon
-            :style="gray"
-            v-show="scope.row.isTop === 1 && scope.row.roofPlacement != 1"
-            @click="roofContent(scope.$index, dataList)"
-            icon-class="icon_ding"
-          />
-        </template>
-      </el-table-column>  
-      <el-table-column prop="sImg" :label="$t(nameMod+'.sImg')" width="100px" show-overflow-tooltip>
-        <template slot-scope="scope">
-          <img :src="scope.row.sImg" class="avatar" width="100px"/>
+          <svg-icon :style="green" v-show="scope.row.isTop === 1 && scope.row.roofPlacement == 1" @click="roofContent(scope.$index, dataList)" icon-class="icon_ping" />
+          <svg-icon :style="gray" v-show="scope.row.isTop === 1 && scope.row.roofPlacement != 1" @click="roofContent(scope.$index, dataList)" icon-class="icon_ding" />
         </template>
       </el-table-column>
-      <el-table-column prop="name" :label="$t('docs.name')" width="150" show-overflow-tooltip>
+      <!-- 名称 -->
+      <el-table-column prop="sImg" :label="$t(nameMod+'.sImg')"  min-width="350" show-overflow-tooltip>
+        <el-row :gutter="20" slot-scope="scope">
+          <el-col :span="12">
+            <el-avatar :src="scope.row.sImg" fit="cover" :size="128"  shape="square" @click="eLink('/'+nameMod+'/'+scope.row._id+'.html')" />
+          </el-col>
+          <el-col :span="12" style="text-align:left">
+            <el-button type="text" size="large" @click="editContentInfo(scope.$index, dataList)">{{scope.row.name}}  <i class="el-icon-edit" /></el-button>
+            <div class="containerTag">
+              <el-tag size="mini" type="success" v-for="artist in scope.row.listRefs" :key="artist._id">{{artist.name}}</el-tag>
+            </div>
+            <!-- <div><span v-for="artist in scope.row.listRefs" :key="artist._id">{{artist.name+','}}</span></div> -->
+            <div >{{scope.row.date}}</div>              
+          </el-col>
+         <!-- <div v-else class="col-name hide" ><el-avatar :src="scope.row.sImg" :fit="cover"/>{{scope.row.name}}</div> -->
+        </el-row>
+      </el-table-column>
+      <!-- 
+      <el-table-column prop="name" :label="$t('docs.name')" min-width="350" show-overflow-tooltip>
         <template slot-scope="scope">
           <a :href="'/'+nameMod+'/'+scope.row._id+'.html'" target="_blank">{{scope.row.name}}</a>
         </template>
       </el-table-column>
-      <el-table-column prop="listRefs" :label="$t(nameMod+'.listArtists')" show-overflow-tooltip>
+       -->
+      <!-- 
+      <el-table-column prop="listRefs" :label="$t(nameMod+'.listArtists')" min-width="200"  show-overflow-tooltip>
         <template slot-scope="scope">
           <span v-for="artist in scope.row.listRefs" :key="artist._id">{{artist.name+','}}</span>
         </template>
       </el-table-column>
+       -->
+      <el-table-column prop="tags" :label="$t('contents.tags')" min-width="200" show-overflow-tooltip>
+        <template slot-scope="scope">
+          <el-tag size="mini" type="info" v-for="tag in scope.row.tags" :key="tag._id">{{tag.name}}</el-tag>
+        </template>
+      </el-table-column>
+
       <el-table-column prop="date" :label="$t(nameMod+'.date')" width="100">
         <template slot-scope="scope">{{scope.row.date}}</template>
       </el-table-column>
@@ -75,168 +73,52 @@
           <span>{{scope.row.listDateDur[0]}}~{{scope.row.listDateDur[1] || ''}}</span>
         </template>
       </el-table-column>
-      <el-table-column prop="state" :label="$t('contents.enable')" width="30" show-overflow-tooltip>
-        <template slot-scope="scope">
-          <svg-icon v-show="scope.row.state=='2'" :style="green" icon-class="check-circle-fill" />
-          <svg-icon v-show="scope.row.state!='2'" :style="red" icon-class="minus-circle-fill" />
-        </template>
-      </el-table-column>
-      <el-table-column prop="clickNum" :label="$t('contents.clickNum')" show-overflow-tooltip></el-table-column>
 
-      
-      <el-table-column :label="$t('main.dataTableOptions')" width="100" fixed="right">
+      <!-- 其他链接 -->
+      <el-table-column prop="listLinks" :label="$t('show.listLinks')" width="80" show-overflow-tooltip>
         <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="primary"
-            plain
-            round
-            @click="editContentInfo(scope.$index, dataList)"
-          >
-            <svg-icon icon-class="edit" />
-          </el-button>
-          <el-button
-            size="mini"
-            type="danger"
-            plain
-            round
-            @click="deleteContent(scope.$index, dataList)"
-          >
-            <svg-icon icon-class="icon_delete" />
-          </el-button>
+          <el-badge :value="scope.row.listLinks.length" :hidden="scope.row.listLinks.length==0?true:false" :max="99" type="info">
+            <el-button @click="eListLinksEdit(scope.$index,dataList)" size="large" plain icon="el-icon-link" :type="scope.row.listLinks.length==0?'':'primary'" circle></el-button>  
+          </el-badge>          
         </template>
-      </el-table-column>
+      </el-table-column>  
+      
+      <!-- 右侧固定栏 -->
+      <el-table-column :label="$t('main.dataTableOptions')" width="200" fixed="right">
+        <template slot-scope="scope">
+          <el-button-group>
+            <el-button icon="el-icon-view" :type="scope.row.state?'':'info'" :disabled="!scope.row.state" size="large" @click="eLink('/'+nameMod+'/'+scope.row._id+'.html')" />
+            <el-button icon="el-icon-edit" size="large" type="success" @click="editContentInfo(scope.$index, dataList)" plain/>
+            <el-button icon="el-icon-delete" size="large" type="danger" plain @click="deleteContent(scope.$index, dataList)" />
+          </el-button-group>
+        </template>
+      </el-table-column>  
     </el-table>
+    <DialogURL @complete="getList" :nameMod="nameMod" :label="$t('show.listLinks')" :dialogState="dialogStateLink" />
+
   </div>
 </template>
 <style lang="scss">
-.fa-star,
-.fa-thumbs-up {
-  cursor: pointer;
-}
-
-.fa-star-o,
-.fa-thumbs-o-up {
-  cursor: pointer;
-}
+  @import "@root/publicMethods/sass/dataTable.scss";
 </style>
 <script>
-import { remove, roof, updateToTop } from "@root/publicMethods/apiGeneral";
-
 import _ from "lodash";
-// import { mapGetters, mapActions,createNamespacedHelpers} from "vuex";
-// let mod = createNamespacedHelpers(nameMod)////模块,含mapGetters, mapActions等
+import {methods,props,data} from "@root/publicMethods/vue/dataTable";
 
 export default {
   props: {
-    nameMod:String,
-    dataList: Array,
-    pageInfo: Object
+    ...props,
   },
   data() {
     return {
-      loading: false,
-      multipleSelection: [],
-      yellow: {
-        color: "#F7BA2A"
-      },
-      gray: {
-        color: "#CCC"
-      },
-      green: { color: "#13CE66" },
-      red: { color: "#FF4949" }
+      ...data,
     };
   },
-
+  components: {
+    DialogURL:() => import("@root/publicMethods/vue/DialogURL.vue"),
+  },
   methods: {
-    // 从mod获取列表更新
-    getList(){
-      this.pageInfo.mod=this.nameMod;
-      this.$store.dispatch(this.nameMod+"/getList", this.pageInfo);
-    },
-    handleContentSelectionChange(val) {
-      if (val && val.length > 0) {
-        let ids = val.map((item, index) => {
-          return item._id;
-        });
-        this.multipleSelection = ids;
-        this.$emit("changeContentSelectList", ids);
-      }
-    },
-    //编辑按钮
-    editContentInfo(index, rows) {
-      let rowData = rows[index];
-      this.$router.push(this.nameMod+"/edit/" + rowData._id);
-    },
-    //推荐
-    topContent(index, rows) {
-      let contentData = rows[index];
-      let targetParams = {
-        _id: contentData._id,
-        isTop: contentData.isTop == 1 ? 0 : 1
-      };
-      updateToTop(targetParams,this.nameMod).then(result => {
-        if (result.status === 200) {
-          this.getList();
-        } else {
-          this.$message.error(result.data.message || result.message);
-        }
-      });
-    },
-    // 置顶
-    roofContent(index, rows) {
-      let contentData = rows[index];
-      // 推荐的才允许置顶
-      if (contentData.isTop != 1) {
-        return false;
-      }
-      let targetParams = {
-        _id: contentData._id,
-        roofPlacement: contentData.roofPlacement == "1" ? "0" : "1"
-      };
-      roof(targetParams,this.nameMod).then(result => {
-        if (result.status === 200) {
-          this.getList();
-        } else {
-          this.$message.error(result.data.message || result.message);
-        }
-      });
-    },
-    deleteContent(index, rows) {
-      this.$confirm(
-        this.$t("main.del_notice"),
-        this.$t("main.scr_modal_title"),
-        {
-          confirmButtonText: this.$t("main.confirmBtnText"),
-          cancelButtonText: this.$t("main.cancelBtnText"),
-          type: "warning"
-        }
-      )
-        .then(() => {
-          return remove({
-            ids: rows[index]._id
-          },this.nameMod);
-        })
-        .then(result => {
-
-          if (result.status === 200) {
-            Object.assign(this.pageInfo);
-            this.getList();
-            this.$message({
-              message: this.$t("main.scr_modal_del_succes_info"),
-              type: "success"
-            });
-          } else {
-            this.$message.error(result.data.message || result.message);
-          }
-        })
-        .catch(() => {
-          this.$message({
-            type: "info",
-            message: this.$t("main.scr_modal_del_error_info")
-          });
-        });
-    }
+    ...methods
   },
   computed: {}
 };
